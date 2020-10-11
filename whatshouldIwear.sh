feaa998e278daeef42b4f1c3fcfcddf2c35a57a2
clemensdotson <56409394+clemensdotson@users.noreply.github.com>
2020-10-11 17:07:35 -0500

    Add files via upload

---
+++ whatshouldIwear.sh
@@ -0,0 +1,7 @@
+
+echo -e "\nWhat Should I Wear Script | Clemens Dotson, 927961\n"
+w=$1; t=$2
+echo -e "The weather today is $w and the temperature is currently $t degrees\n"
+echo "What should I wear today?"
+read clothes
+echo "Sounds good, I'll wear $clothes today."
